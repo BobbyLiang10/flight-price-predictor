24ea6e0b3e01afb6132efe034ca887dcdca99b51
Bobby Liang <83838450+BobbyLiang10@users.noreply.github.com>
2025-03-31 23:21:04 -0400

    Add files via upload

--- test3.py
+++ test3.py
@@ -4,15 +4,20 @@ import pandas as pd
 import numpy as np
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 import os
 
 options = webdriver.ChromeOptions()
+ua = UserAgent()
+user_agent = ua.random
+print(user_agent)
+#options.add_argument(f'--user-agent={user_agent}')
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 
 
 
-origins = ['YYZ', 'MAN', 'CDG', 'ZRH', 'JFK', 'LHR', 'BCN', 'BER', 'HKG', 'FCO']
+origins = ['MAN', 'CDG', 'ZRH', 'JFK', 'LHR', 'BCN', 'BER', 'HKG', 'FCO']
 mapped_list = []
 for i in range(len(origins)):
     for j in range(len(origins)):
@@ -55,27 +60,46 @@ def get_prices(soup):
 column_names = ["Airline", "Origin", "Destination","Duration","Total stops", "Price","Flight date","Days left until flight"]
 df = pd.DataFrame(columns = column_names)
 
+# counter to store number of times we switched pages
+counter = 0
+
+prices = []
+airlines = []
+origins = []
+destinations = []
+flight_dates = []
+stops = []
+durations = []
+days_left = []
+
 # iterating through all flight paths
 for journey in mapped_list:
     origin = journey[0]
     destination = journey[1]
+    # initializing and resetting the list of important characteristics
+    df = pd.DataFrame(columns = column_names)
+    prices = []
+    airlines = []
+    origins = []
+    destinations = []
+    flight_dates = []
+    stops = []
+    durations = []
+    days_left = []
 # retrieving flight info for the given time range
     for i in range(num_days+1):
+        counter += 1
+        
+        # close and open driver every 10 searches to avoid captcha
+        if counter % 10 == 0:
+            driver.quit()
+            driver = webdriver.Chrome(options=options)#, chrome_options=chromeOptions)
+        
         url = f'https://www.ca.kayak.com/flights/{origin}-{destination}/{start_date+i}?ucs=j3cp93&sort=bestflight_a'
         driver.get(url)
         sleep(10)
         flight_rows = driver.find_elements("xpath", '//div[@class="Fxw9-result-item-container"]')
-        
-        # initializing and resetting the list of important characteristics
-        prices = []
-        airlines = []
-        origins = []
-        destinations = []
-        flight_dates = []
-        stops = []
-        durations = []
-        days_left = []
-        
+
         for WebElement in flight_rows:
             elementHTML = WebElement.get_attribute('outerHTML')
             elementSoup = BeautifulSoup(elementHTML, 'html.parser')
@@ -85,6 +109,7 @@ for journey in mapped_list:
             if temp_price is not None:
                 price = temp_price.find("div", {"class": "f8F1-price-text"})
                 prices.append(price.text)
+                flight_dates.append(datetime.strftime(start_date + timedelta(days=i), "%Y-%m-%d"))
             
             # retrieving airlines
             temp_airline = elementSoup.find("div", {"class": "nrc6-content-section"})
@@ -102,21 +127,23 @@ for journey in mapped_list:
             temp_duration = elementSoup.find("div", {"class": "nrc6-content-section"})
             if temp_duration is not None:
                 duration = temp_duration.find("div", {"class": "xdW8 xdW8-mod-full-airport"})
-                print(duration)
                 durations.append(duration.text)
 
-        flight_date = datetime.strftime(start_date + timedelta(days=i), "%Y-%m-%d")
-        for j in range(len(prices)):
-            df = pd.concat([df, pd.DataFrame([{
-                'Airline': airlines[j],
-                'Duration': durations[j],
-                'Total stops': stops[j],
-                'Price': prices[j],
-                'Flight date': flight_date,
-                'Days left until flight': abs((datetime.strptime(flight_date, "%Y-%m-%d")-
-                                            (datetime.strptime(today, "%Y-%m-%d"))).days)
-                }])], ignore_index=True)
-        
+            #flight_dates.append(datetime.strftime(start_date + timedelta(days=i), "%Y-%m-%d"))
+            #print("In Webelement loop: \n", prices)
+    
+    #print("In num days loop \n:", prices)
+    for j in range(len(prices)):
+        df = pd.concat([df, pd.DataFrame([{
+            'Airline': airlines[j],
+            'Duration': durations[j],
+            'Total stops': stops[j],
+            'Price': prices[j],
+            'Flight date': flight_dates[j],
+            'Days left until flight': abs((datetime.strptime(flight_dates[j], "%Y-%m-%d")-
+                                        (datetime.strptime(today, "%Y-%m-%d"))).days)
+        }])], ignore_index=True)
+    
     df['Origin'] = origin
     df['Destination'] = destination
     df['Booking date'] = today
